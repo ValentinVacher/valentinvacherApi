53d46654184d649610f9faaf6312911407d0a8d0
Valentin Vacher <108465376+ValentinVacher@users.noreply.github.com>
2024-07-17 13:17:05 +0200

    Initial commit

---
+++ src/test/java/com/valentivacher/api/ValentinvacherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valentivacher.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValentinvacherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
