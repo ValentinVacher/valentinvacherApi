6c73a965c39d879beaed898dce3cd4c98de2886b
Valentin Vacher <108465376+ValentinVacher@users.noreply.github.com>
2024-07-27 13:55:02 +0200

    documentation config

---
+++ src/main/java/com/valentivacher/api/config/ApiDocumentationConfiguration.java
@@ -0,0 +1,27 @@
+package com.valentivacher.api.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class ApiDocumentationConfiguration {
+    @Bean
+    public OpenAPI apiDoConfig() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("valentin-vacher.fr")
+                        .description("Api de valentin-vacher.fr")
+                        .version("1.0.0"));
+
+    }
+}
